b691afc9e7a368a2ef14aea3aa0f84fe142236f6
bodik1987 <bsshul@gmail.com>
2025-01-30 12:13:33 +0100

    PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,36 @@
 import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Калории",
+        short_name: "Калории",
+        description: "Калории",
+        theme_color: "#E5E5E5",
+        orientation: "portrait",
+        icons: [
+          {
+            src: "icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+    }),
+  ],
 });
