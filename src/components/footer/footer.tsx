4064c1b37d15567b953413caf2beceffedbac324
bodik1987 <bsshul@gmail.com>
2025-01-31 23:00:00 +0100

    Fix

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -8,11 +8,11 @@ export default function Footer({}: FooterProps) {
   const { selectedDay, setSelectedDay } = useSelectedDayStore();
   return (
     <footer className="fixed bottom-0 inset-x-0 h-16 bg-panel dark:bg-dark-panel select-none">
-      <div className="container h-full font-medium flex items-center justify-around">
+      <div className="container h-full flex items-center justify-around">
         <button
           onClick={() => setSelectedDay(1)}
           aria-label="День"
-          className={`w-1/3 button ${
+          className={`w-1/3 !h-10 button ${
             selectedDay === 1 &&
             location.pathname === "/" &&
             "bg-accent dark:bg-dark-accent text-white"
@@ -24,7 +24,7 @@ export default function Footer({}: FooterProps) {
         <button
           onClick={() => setSelectedDay(2)}
           aria-label="День"
-          className={`w-1/3 button ${
+          className={`w-1/3 !h-10 button ${
             selectedDay === 2 &&
             location.pathname === "/" &&
             "bg-accent dark:bg-dark-accent text-white"
