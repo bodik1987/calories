5577796e1d97938280286584db5e0a0ac3d48258
bodik1987 <bsshul@gmail.com>
2025-02-12 10:17:53 +0100

    Fix

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -10,7 +10,7 @@ export default function Footer() {
 
   return (
     <footer className="fixed bottom-0 inset-x-0 pt-3 pb-8 bg-panel/70 backdrop-blur-md dark:bg-dark-panel select-none border-t border-accent/5">
-      <div className="container h-full flex items-center justify-around gap-4 pl-4 pr-7">
+      <div className="container h-full flex items-center justify-around gap-4 px-4">
         {[1, 2].map((day) => (
           <button
             key={day}
@@ -34,7 +34,7 @@ export default function Footer() {
             setOpenBottomSheet(true);
             setContentKey("list");
           }}
-          className="ml-5 button w-12 aspect-square bg-accent dark:bg-dark-accent text-white z-10"
+          className="px-6 button bg-accent dark:bg-dark-accent text-white z-10"
           aria-label="Добавить"
         >
           <ChewronUpIcon />
