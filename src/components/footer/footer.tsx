1319ac4fe40b1d4a1dcc519399d9145435f8e5e0
bodik1987 <bsshul@gmail.com>
2025-03-01 22:49:34 +0100

    Text and buttons

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -11,30 +11,33 @@ export default function Footer() {
   return (
     <footer className="fixed bottom-0 inset-x-0 pt-3 pb-8 bg-panel/70 backdrop-blur-md dark:bg-dark-panel select-none border-t border-accent/5">
       <div className="container h-full flex items-center justify-around gap-4 px-3">
-        {[1, 2].map((day) => (
-          <button
-            key={day}
-            onClick={() => setSelectedDay(day)}
-            aria-label="День"
-            className={`w-full button !h-11 ${
-              selectedDay === day && "bg-accent dark:bg-dark-accent text-white"
-            } text-accent dark:text-neutral-50`}
-          >
-            {day === 1
-              ? isTodayOdd
-                ? "Сегодня"
-                : "Завтра"
-              : isTodayOdd
-              ? "Завтра"
-              : "Сегодня"}
-          </button>
-        ))}
+        <div className="w-full bg-white dark:bg-white/5 rounded-full p-2 flex">
+          {[1, 2].map((day) => (
+            <button
+              key={day}
+              onClick={() => setSelectedDay(day)}
+              aria-label="День"
+              className={`w-full button !text-lg !h-11 ${
+                selectedDay === day &&
+                "bg-accent dark:bg-dark-accent !text-white"
+              } text-accent dark:text-neutral-400`}
+            >
+              {day === 1
+                ? isTodayOdd
+                  ? "Сегодня"
+                  : "Завтра"
+                : isTodayOdd
+                ? "Завтра"
+                : "Сегодня"}
+            </button>
+          ))}
+        </div>
         <button
           onClick={() => {
             setOpenBottomSheet(true);
             setContentKey("list");
           }}
-          className="px-6 button !h-11 bg-accent dark:bg-dark-accent text-white z-10"
+          className="button !w-14 aspect-square !h-14 bg-accent dark:bg-dark-accent text-white z-10"
           aria-label="Добавить"
         >
           <ChewronUpIcon />
